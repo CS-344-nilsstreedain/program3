377394f85c2287d7ed3097d443038652fcdda9ed
Nils Streedain <nils@streedain.com>
2023-02-20 19:23:33 -0800

    Cleanup

--- main.c
+++ main.c
@@ -11,35 +11,65 @@
 #define MAX_ARGS 512
 
 int fgMode = 0;
+pid_t bgPids[200];
+int numBgPids = 0;
+int lastStatus = 0;
 
 struct command {
-	char *cmd;
-	char *args[MAX_ARGS];
-	char *builtArgs[MAX_ARGS];
-	char *inputFile;
-	char *outputFile;
-	int background;
-	int redirect;
+	char *args[MAX_ARGS + 1];
+	int argc;
+	char *inFile;
+	char *outFile;
+	int bg;
 };
 
-void promptUser(char* line, size_t lineSize) {
+int parseCmd(char* line, struct command* cmd) {
 	printf(": ");
 	fflush(stdout);
-	getline(&line, &lineSize, stdin);
+	size_t bufsize = MAX_CMD_LENGTH;
+	getline(&line, &bufsize, stdin);
+
+	// Continue if empty or comment
+	if (line[0] == '\n' || line[0] == '#')
+		return 1;
 	
-	// Remove newline character from end of input
+	// Remove newline and & character from end of input
 	line[strcspn(line, "\n")] = '\0';
+	if (line[strlen(line) - 1] == '&') {
+		line[strlen(line) - 1] = '\0';
+		cmd->bg = 1;
+	}
+	
+	// Expand PID
+	char buf[MAX_CMD_LENGTH];
+	for (char* ptr; (ptr = strstr(line, "$$")); ) {
+		*ptr = '\0';
+		sprintf(buf, "%s%d%s", line, getpid(), ptr + 2);
+		strcpy(line, buf);
+	}
+	
+	// Tokenize
+	char* token = strtok(line, " ");
+	while (token != NULL && cmd->argc < MAX_ARGS) {
+		if (!strcmp(token, "<"))
+			cmd->inFile = strtok(NULL, " ");
+		else if (!strcmp(token, ">"))
+			cmd->outFile = strtok(NULL, " ");
+		else
+			cmd->args[cmd->argc++] = token;
+		token = strtok(NULL, " ");
+	}
+	return 0;
 }
 
-void expandPid(char* str) {
-	char* ptr;
-	char buf[MAX_CMD_LENGTH];
-
-	while ((ptr = strstr(str, "$$"))) {
-		*ptr = '\0';
-		sprintf(buf, "%s%d%s", str, getpid(), ptr + 2);
-		strcpy(str, buf);
-	}
+void runCd(struct command cmd) {
+	if (cmd.argc > 1) {
+		if (chdir(cmd.args[1])) {
+			fprintf(stderr, "Could not find %s\n", cmd.args[1]);
+			fflush(stdout);
+		}
+	} else
+		chdir(getenv("HOME"));
 }
 
 void runStatus(int lastStatus) {
@@ -48,6 +78,28 @@ void runStatus(int lastStatus) {
 	else if (WIFSIGNALED(lastStatus))
 		printf("terminated by signal %d\n", WTERMSIG(lastStatus));
 	fflush(stdout);
+}
+
+void checkBg(int* bgPids, int* numBgPids, int* status, int terminate) {
+	for (int i = 0; i < *numBgPids; i++) {
+		pid_t pid = waitpid(bgPids[i], status, WNOHANG);
+		
+		if (terminate)
+			kill(bgPids[i], SIGKILL);
+		
+		if (pid != 0) {
+			printf("background pid %d is done: ", bgPids[i]);
+			fflush(stdout);
+			runStatus(*status);
+			for (int j = i; j < *numBgPids - 1; j++)
+			   bgPids[j] = bgPids[j + 1];
+			
+			bgPids[*numBgPids] = 0;
+			(*numBgPids)--;
+		}
+	}
+	if (terminate)
+		exit(0);
 }
 
 void redirectIO(char* file, int redirFd, int flags, char* action) {
@@ -63,25 +115,45 @@ void redirectIO(char* file, int redirFd, int flags, char* action) {
 	}
 }
 
-void checkBg(int* bgPids, int* numBgPids, int* status, int terminate) {
-	for (int i = 0; i < *numBgPids; i++) {
-		pid_t pid = waitpid(bgPids[i], status, WNOHANG);
+void newProcess(struct command cmd, struct sigaction sa) {
+	// Fork for new process
+	pid_t pid = fork();
+	
+	if (pid == -1) {
+		perror("fork() failed!");
+		exit(1);
+	} else if (pid == 0) {
+		// Child process
+		if (!cmd.bg) {
+			sa.sa_handler = SIG_DFL;
+			sa.sa_flags = 0;
+			sigaction(SIGINT, &sa, NULL);
+		}
 		
-		if (terminate)
-			kill(bgPids[i], SIGKILL);
-		
-		if (pid != 0) {
-			printf("\nbackground pid %d is done: ", bgPids[i]);
+		redirectIO(cmd.inFile, 0, O_RDONLY, "input");
+		redirectIO(cmd.outFile, 1, O_WRONLY | O_CREAT | O_TRUNC, "output");
+		execvp(cmd.args[0], cmd.args);
+		fprintf(stderr, "%s: no such file or directory\n", cmd.args[0]);
+		fflush(stdout);
+		exit(1);
+	} else {
+		// In the parent process
+		if (cmd.bg && !fgMode) {
+			// Print background process ID
+			bgPids[numBgPids++] = pid;
+			printf("background pid is %d\n", pid);
 			fflush(stdout);
-			runStatus(*status);
-			for (int j = i; j < *numBgPids - 1; j++)
-			   bgPids[j] = bgPids[j + 1];
-			
-			bgPids[*numBgPids] = 0;
-			(*numBgPids)--;
+		} else {
+			// Wait for child process to complete
+			waitpid(pid, &lastStatus, 0);
 		}
 	}
+	
+	// Reset file descriptors
+	if (cmd.inFile || cmd.outFile)
+		dup2(0, 0), dup2(1, 1);
 }
+
 void set_signal_handler(int signum, void (*handler)(int), int flags, struct sigaction *sa) {
 	sa->sa_handler = handler;
 	sigfillset(&sa->sa_mask);
@@ -97,108 +169,37 @@ void handle_SIGTSTP(int signo) {
 }
 
 int main(int argc, const char * argv[]) {
-	struct sigaction SIGINT_action = {0}, SIGTSTP_action = {0};
-	set_signal_handler(SIGINT, SIG_IGN, 0, &SIGINT_action);
-	set_signal_handler(SIGTSTP, handle_SIGTSTP, SA_RESTART, &SIGTSTP_action);
+	struct sigaction sa = {0}, SIGTSTP_action = {0};
+	set_signal_handler(SIGINT, SIG_IGN, 0, &sa);
+	set_signal_handler(SIGTSTP, handle_SIGTSTP, SA_RESTART, &sa);
 	
 	char line[MAX_CMD_LENGTH];
-	pid_t bgPids[200];
-	int numBgPids = 0;
-	int lastStatus = 0;
 	
 	while(1) {
 		checkBg(bgPids, &numBgPids, &lastStatus, 0);
-		promptUser(line, MAX_CMD_LENGTH);
-		if (strlen(line) == 0 || line[0] == '#')
-			continue;
 		
-		// Expand $$ variable
-		expandPid(line);
+		// Init command struct
+		struct command cmd = {
+			.args = { NULL },
+			.argc = 0,
+			.inFile = NULL,
+			.outFile = NULL,
+			.bg = 0
+		};
 		
-		// Init command vars
-		int argCount = 0;
-		char* args[MAX_ARGS + 1] = { NULL };
-		char* inputFile = NULL;
-		char* outputFile = NULL;
-		
-		char* token = strtok(line, " ");
-		// Tokenize input
-		while (token != NULL && argCount < MAX_ARGS) {
-			if (!strcmp(token, "<"))
-				inputFile = strtok(NULL, " ");
-			else if (!strcmp(token, ">"))
-				outputFile = strtok(NULL, " ");
-			else
-				args[argCount++] = token;
-			token = strtok(NULL, " ");
-		}
-		
-		int background = 0;
-		if (argCount > 0 && !strcmp(args[argCount - 1], "&")) {
-			background = 1;
-			args[argCount - 1] = NULL;
-			argCount--;
-		}
-		
-		if (!argCount)
+		// Parse command and skip if error or empty
+		if (parseCmd(line, &cmd) || !cmd.argc)
 			continue;
 		
 		// Handle built-in commands
-		if (!strcmp(args[0], "exit")) {
+		if (!strcmp(cmd.args[0], "exit"))
 			checkBg(bgPids, &numBgPids, &lastStatus, 1);
-			exit(0);
-		} else if (!strcmp(args[0], "cd")) {
-			if (argCount > 1) {
-				if (chdir(args[1])) {
-					fprintf(stderr, "Could not find %s\n", args[1]);
-					fflush(stdout);
-				}
-			} else {
-				chdir(getenv("HOME"));
-			}
-		} else if (!strcmp(args[0], "status")) {
+		else if (!strcmp(cmd.args[0], "cd"))
+			runCd(cmd);
+		else if (!strcmp(cmd.args[0], "status"))
 			runStatus(lastStatus);
-		} else {
-			// Fork for new process
-			pid_t pid = fork();
-			
-			if (pid == -1) {
-				perror("fork() failed!");
-				exit(1);
-			} else if (pid == 0) {
-				// Child process
-				if (!background) {
-					SIGINT_action.sa_handler = SIG_DFL;
-					SIGINT_action.sa_flags = 0;
-					sigaction(SIGINT, &SIGINT_action, NULL);
-				}
-				
-				redirectIO(inputFile, 0, O_RDONLY, "input");
-				redirectIO(outputFile, 1, O_WRONLY | O_CREAT | O_TRUNC, "output");
-				execvp(args[0], args);
-				fprintf(stderr, "%s: no such file or directory\n", args[0]);
-				fflush(stdout);
-				exit(1);
-			} else {
-				// In the parent process
-				if (background && !fgMode) {
-					// Print background process ID
-					bgPids[numBgPids++] = pid;
-					printf("background pid is %d\n", pid);
-					fflush(stdout);
-				} else {
-					// Wait for child process to complete
-					waitpid(pid, &lastStatus, 0);
-				}
-			}
-			
-			// Reset file descriptors
-			if (inputFile != NULL)
-				dup2(0, 0);
-
-			if (outputFile != NULL)
-				dup2(1, 1);
-		}
+		else
+			newProcess(cmd, sa);
 	}
 	return 0;
 }
